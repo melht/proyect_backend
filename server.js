e027982bdc450b7e2810740b8562db5a9f93189f
Leonardo Xicotencatl <llxicotencatlg@hotmail.com>
2024-05-14 01:54:01 -0600

    Cors agregation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const colors = require('colors')
+const cors = require('cors')
 const connectDB = require('./config/db.js')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -12,6 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors())
 
 app.use('/api/horoscopo', require('./routes/HoroscopoRoutes.js')) 
 app.use('/api/users', require('./routes/userRoutes.js'))
